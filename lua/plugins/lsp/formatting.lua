27f88c0b3dae6a9caa758c1b3d55037396505e17
Piotr Anisko <github@anisko.info>
2026-08-21 12:00:34 +0200

    fixed nvim-lint error

--- lua/plugins/lsp/formatting.lua
+++ lua/plugins/lsp/formatting.lua
@@ -50,18 +50,44 @@ return {
         lua = { 'luacheck' },
       }
 
+      -- Run only linters whose executable is available on PATH
+      local function try_lint_available()
+        if not vim.opt_local.modifiable:get() then
+          return
+        end
+
+        local ft = vim.bo.filetype
+        local linter_names = lint._resolve_linter_by_ft(ft)
+        local names_to_run = {}
+
+        for _, name in ipairs(linter_names) do
+          local linter = lint.linters[name]
+          if linter then
+            local cmd = type(linter) == 'table' and linter.cmd or name
+            if type(cmd) == 'function' then
+              cmd = cmd()
+            end
+            if type(cmd) == 'string' and vim.fn.executable(cmd) == 1 then
+              table.insert(names_to_run, name)
+            end
+          end
+        end
+
+        if #names_to_run > 0 then
+          lint.try_lint(names_to_run)
+        end
+      end
+
       -- Setup autocommand for linting
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
-          pcall(lint.try_lint)
+          try_lint_available()
         end,
       })
 
-      vim.keymap.set('n', '<leader>tl', function()
-        lint.try_lint()
-      end, { desc = '[T]oggle [L]int' })
+      vim.keymap.set('n', '<leader>tl', try_lint_available, { desc = '[T]oggle [L]int' })
     end,
   },
 }
